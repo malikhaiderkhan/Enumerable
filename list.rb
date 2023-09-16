c8689d8431db1e5d77fde370f9c4ab1ae95b831f
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2023-09-16 08:53:43 +0100

    Update list.rb

--- list.rb
+++ list.rb
@@ -11,3 +11,11 @@ include MyEnumerable
     @list.each { |item| yield item }
   end
 end
+
+list = MyList.new([1, 2, 3, 4])
+puts list.all? { |e| e < 5 }
+puts list.all? { |e| e > 5 }
+puts list.any? { |e| e == 2 }
+puts list.any? { |e| e == 5 }
+puts list.filter { |e| e.even? }
+list.each { |e| print "#{e} " }
